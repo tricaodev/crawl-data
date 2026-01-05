8239832c351837fccafb1c62075a5ce578670041
Tri Cao <tricao.dev@gmail.com>
2026-01-05 20:44:05 +0700

    update code

--- utils.py
+++ utils.py
@@ -30,6 +30,7 @@ def make_driver(headless=False):
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
         "(KHTML, like Gecko) Chrome/120.0 Safari/537.36"
     )
+    opts.page_load_strategy = "eager"
 
     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opts)
 
